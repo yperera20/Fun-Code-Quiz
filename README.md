f53afee6be3f623d911cfbc3db07c80a6809a7a0
yperera20 <buddhinperera@yahoo.com>
2023-09-14 00:04:48 +1000

    update-1

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Fun-Code-Quiz
+# Code Quiz Web App
+Welcome to the Code Quiz web application! This interactive quiz allows users to test their coding knowledge by answering a series of questions and saving their scores. Users can also view high scores to see how they compare to others.
+
+Getting Started
+Prerequisites
+To run this web application, you need a modern web browser that supports HTML, CSS, and JavaScript.
+
+Usage
+Open the web application in your browser.
+
+Click the "Start Quiz" button to begin the quiz.
+
+Answer the questions by selecting the correct option.
+
+If you answer incorrectly, time will be deducted from the timer.
+
+At the end of the quiz or when the timer reaches 0, the game is over.
+
+Enter your initials and click the "Save Score" button to save your score.
+
+High Scores
+You can view high scores by clicking the "View Scores" button on the main page. Scores are saved in local storage, so they will persist even if you close the browser.
+
+Website :
+To access the password generator:
+
+![Alt text](image.png)
---
+++ assets/js/script.js
@@ -0,0 +1,116 @@
+var startBtn = document.getElementById('start-btn');
+var quizContainer = document.getElementById('quiz-container');
+var questionElement = document.getElementById('question');
+var optionsElement = document.getElementById('options');
+var resultElement = document.getElementById('result');
+var timerElement = document.getElementById('timer');
+var timeSpan = document.getElementById('time');
+var initialsInput = document.getElementById('initials');
+var saveScoreBtn = document.getElementById('save-score-btn');
+
+let currentQuestionIndex = 0;
+let score = 0;
+let timeLeft = 60;
+let timerInterval;
+
+const questions = [
+    {
+        question: 'What is HTML?',
+        options: ['Hypertext Markup Language', 'Hyper Transfer Markup Language', 'Hyper Text Makeup Language', 'High Text Markup Language'],
+        answer: 0
+    },
+    {
+        question: 'What is CSS?',
+        options: ['Cascading Style Sheet', 'Computer Style Sheet', 'Creative Style Sheet', 'Colorful Style Sheet'],
+        answer: 0
+    },
+    {
+        question: 'What is JavaScript?',
+        options: ['Just Another Scripting Language', 'Java Super Text', 'JavaScript', 'Joke and Script'],
+        answer: 2
+    }
+];
+
+startBtn.addEventListener('click', startQuiz);
+saveScoreBtn.addEventListener('click', saveScore);
+
+function startQuiz() {
+    startBtn.style.display = 'none';
+    quizContainer.style.display = 'block';
+    setNextQuestion();
+    startTimer();
+}
+
+function startTimer() {
+    timerInterval = setInterval(function () {
+        timeLeft--;
+        timeSpan.textContent = timeLeft;
+
+        if (timeLeft <= 0) {
+            endQuiz();
+        }
+    }, 1000);
+}
+
+function setNextQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        const question = questions[currentQuestionIndex];
+        questionElement.textContent = question.question;
+        optionsElement.innerHTML = '';
+
+        question.options.forEach((option, index) => {
+            const button = document.createElement('button');
+            button.textContent = option;
+            button.addEventListener('click', () => selectOption(index));
+            optionsElement.appendChild(button);
+        });
+    } else {
+        endQuiz();
+    }
+}
+
+function selectOption(selectedIndex) {
+    const question = questions[currentQuestionIndex];
+
+    if (selectedIndex === question.answer) {
+        score++;
+        resultElement.textContent = 'Correct!';
+    } else {
+        timeLeft -= 10; // Subtract 10 seconds for incorrect answers
+        resultElement.textContent = 'Incorrect!';
+    }
+
+    currentQuestionIndex++;
+    setNextQuestion();
+}
+
+function endQuiz() {
+    clearInterval(timerInterval);
+    quizContainer.style.display = 'none';
+    initialsInput.style.display = 'block';
+    saveScoreBtn.style.display = 'block';
+}
+
+// Saving the score
+function saveScore() {
+    const initials = initialsInput.value;
+    if (initials.trim() !== '') {
+
+        const scoreElement = document.getElementById('Score');
+        scoreElement.textContent = score;
+
+        alert(`Score saved: ${score}`);
+        
+    } else {
+        alert('Please enter your initials.');
+    }
+}
+// Function to retrieve and display scores
+function retrieveAndDisplayScores() {
+    const savedScores = JSON.parse(localStorage.getItem('scores')) || scores;
+    savedScores.sort((a, b) => b.score - a.score);
+
+}
+
+const viewScoresBtn = document.getElementById('view-scores-btn');
+viewScoresBtn.addEventListener('click', retrieveAndDisplayScores);
